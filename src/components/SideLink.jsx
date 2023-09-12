b94a058f9a9b841872bc5af8c640207f99ec79b3
smile1130 <superskiller1130@gmail.com>
2023-09-12 10:41:45 +0900

    Update sidelink style

--- src/components/SideLink.jsx
+++ src/components/SideLink.jsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 
 export function SideLink({ href, children }) {
+    const router = useRouter();
     return (
         <div>
             <Link
                 href={href}
-                className="flex flex-row  gap-2 inline-block rounded-lg py-1 px-2 mb-2 text-sm text-slate-700 hover:bg-slate-300 hover:text-slate-700 dark:text-slate-300 dark:hover:bg-slate-600"
+                className={`flex flex-row  gap-2 rounded-lg py-1 px-2 mb-2 text-sm text-slate-700 hover:bg-slate-300 hover:text-slate-700 dark:text-slate-300 dark:hover:bg-slate-600 ${(href == router.asPath) ? "active" : ""}`}
             >
                 {children}
             </Link>
