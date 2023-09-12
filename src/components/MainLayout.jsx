cc735b5041f6ad3c4e994c73b7f31732dced970d
smile1130 <superskiller1130@gmail.com>
2023-09-12 18:25:38 +0900

    Add PrimaryFeatures

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,7 +5,7 @@ export function MainLayout({ children }) {
         <>
             <div className="flex flex-row max-md:flex-col">
                 <Sidebar />
-                <div className='bg-zinc-100 dark:bg-zinc-800 h-screen w-full pt-8 px-4'>
+                <div className='bg-zinc-100 dark:bg-zinc-800 h-screen max-md:h-[calc(100vh-88px)] w-full pt-8 px-4'>
                     {children}
                 </div>
             </div>
--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -1,46 +1,56 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-features.jpg'
-import screenshotExpenses from '@/images/screenshots/expenses.png'
-import screenshotPayroll from '@/images/screenshots/payroll.png'
-import screenshotReporting from '@/images/screenshots/reporting.png'
-import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
+import screenshotRegister from '@/images/screenshots/register.png'
+import screenshotCompanies from '@/images/screenshots/companies.png'
+import screenshotModels from '@/images/screenshots/models.png'
+import screenshotResearch from '@/images/screenshots/research.png'
+import screenshotRegister_dark from '@/images/screenshots/register_dark.png'
+import screenshotCompanies_dark from '@/images/screenshots/companies_dark.png'
+import screenshotModels_dark from '@/images/screenshots/models_dark.png'
+import screenshotResearch_dark from '@/images/screenshots/research_dark.png'
 
 const features = [
   {
-    title: 'Payroll',
+    title: 'Register/Login',
     description:
-      "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
-    image: screenshotPayroll,
+      "Your sidekick will create a customized account plan for each prospect account based on key information.",
+    image: screenshotRegister,
+    image_dark: screenshotRegister_dark,
   },
   {
-    title: 'Claim expenses',
+    title: 'Companies',
     description:
-      "All of your receipts organized into one place, as long as you don't mind typing in the data by hand.",
-    image: screenshotExpenses,
+      "Your sidekick will create a customized account plan for each prospect account based on key information.",
+    image: screenshotCompanies,
+    image_dark: screenshotCompanies_dark,
   },
   {
-    title: 'VAT handling',
+    title: 'Train AI',
     description:
-      "We only sell our software to companies who don't deal with VAT at all, so technically we do all the VAT stuff they need.",
-    image: screenshotVatReturns,
+      "Your sidekick will create a customized account plan for each prospect account based on key information.",
+    image: screenshotModels,
+    image_dark: screenshotModels_dark,
   },
   {
-    title: 'Reporting',
+    title: 'Research',
     description:
-      'Easily export your data into an Excel spreadsheet where you can do whatever the hell you want with it.',
-    image: screenshotReporting,
+      'Your sidekick will create a customized account plan for each prospect account based on key information.',
+    image: screenshotResearch,
+    image_dark: screenshotResearch_dark,
   },
 ]
 
 export function PrimaryFeatures() {
   let [tabOrientation, setTabOrientation] = useState('horizontal')
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    setIsDarkMode(localStorage.getItem('isDarkMode'));
     let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
 
     function onMediaQueryChange({ matches }) {
@@ -59,24 +69,15 @@ export function PrimaryFeatures() {
     <section
       id="features"
       aria-label="Features for running your books"
-      className="relative overflow-hidden bg-blue-600 pb-28 pt-20 sm:py-32"
+      className="relative overflow-hidden pb-28 pt-20 sm:py-32 bg-slate-100 dark:bg-zinc-900"
     >
-      <Image
-        className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
+          <h2 className="font-display text-3xl tracking-tight dark:text-slate-300 text-slate-900 sm:text-4xl md:text-5xl">
+            Everything you need to use this service.
           </h2>
-          <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
+          <p className="mt-6 text-lg tracking-tight dark:text-slate-300 text-slate-900">
+            Well everything you need is very simple.
           </p>
         </div>
         <Tab.Group
@@ -94,8 +95,8 @@ export function PrimaryFeatures() {
                       className={clsx(
                         'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
                         selectedIndex === featureIndex
-                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
-                          : 'hover:bg-white/10 lg:hover:bg-white/5'
+                          ? 'lg:bg-slate-300 lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10 dark:bg-slate-800'
+                          : 'dark:hover:bg-white/10 dark:lg:hover:bg-white/5 hover:bg-slate-200'
                       )}
                     >
                       <h3>
@@ -103,8 +104,8 @@ export function PrimaryFeatures() {
                           className={clsx(
                             'font-display text-lg [&:not(:focus-visible)]:focus:outline-none',
                             selectedIndex === featureIndex
-                              ? 'text-blue-600 lg:text-white'
-                              : 'text-blue-100 hover:text-white lg:text-white'
+                              ? 'dark:text-blue-600 dark:lg:text-white text-blue-900 font-bold lg:text-slate-700'
+                              : 'dark:text-blue-100 dark:hover:text-white dark:lg:text-white text-slate-700 group-hover:font-bold'
                           )}
                         >
                           <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
@@ -115,8 +116,8 @@ export function PrimaryFeatures() {
                         className={clsx(
                           'mt-2 hidden text-sm lg:block',
                           selectedIndex === featureIndex
-                            ? 'text-white'
-                            : 'text-blue-100 group-hover:text-white'
+                            ? 'text-slate-900 dark:text-white'
+                            : 'text-slate-400 dark:group-hover:text-slate-200 group-hover:text-slate-700'
                         )}
                       >
                         {feature.description}
@@ -130,14 +131,14 @@ export function PrimaryFeatures() {
                   <Tab.Panel key={feature.title} unmount={false}>
                     <div className="relative sm:px-6 lg:hidden">
                       <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
-                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                      <p className="relative mx-auto max-w-2xl text-base dark:text-white text-slate-700 sm:text-center">
                         {feature.description}
                       </p>
                     </div>
                     <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
                       <Image
                         className="w-full"
-                        src={feature.image}
+                        src={!isDarkMode? feature.image : feature.image_dark}
                         alt=""
                         priority
                         sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
